d7b90035a6726d0200078426604dbc4c1f8dc318
Ahmed Ahmed <ahmed.ahmed.jobb@gmail.com>
2026-01-29 15:14:42 +0100

    Enable Swagger in production

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -30,12 +30,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
